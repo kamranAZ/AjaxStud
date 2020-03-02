5964614619b374f833bbe3e62843424b8f87a18c
kamranAZ <dearzade@gmail.com>
2020-03-02 22:00:35 +0300

    Ajax

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using studAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        stAjQ_Db db = new stAjQ_Db();
+
+        public ActionResult Index()
+        {
+            ViewBag.StuAjax = db.Studentsses.Take(3).ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/MainAjaxController.cs
@@ -0,0 +1,38 @@
+﻿using studAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studAjax.Controllers
+{
+    public class MainAjaxController : Controller
+    {
+        stAjQ_Db db = new stAjQ_Db();
+        // GET: MainAjax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StudentList(int take, int skip)
+        {
+            object data = null;
+            var modelist = db.Studentsses.OrderBy(st => st.StudentName).Skip(skip).Take(take).Select(st => new
+            {
+                st.Id,
+                st.StudentName,
+                st.StudentEmail,
+                st.StudentPhone
+            }).ToList();
+            data = new
+            {
+                status = 200,
+                message = "succes",
+                response = modelist
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
